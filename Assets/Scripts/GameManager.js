dcdfff83551a44a9eb508422b3e60b3f8c9084b3
Erbarbar <filipelopez3@gmail.com>
2017-04-10 14:54:19 +0100

    Initial Commit

---
+++ Assets/Scripts/GameManager.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+public var timer : float = 0;
+public var timeLimit : float = 10;
+public var overTime : boolean = false;
+
+public 
+
+public var text_time: Text;
+
+function Start () {
+	timer = timeLimit;
+}
+
+function Update () {
+    getTime();
+    showTime(timer);
+}
+
+function getTime(){
+	if(!overTime){
+		timer -= Time.deltaTime;
+		if(timer <=0){
+			overTime = true;
+		}
+	}else{
+		timer += Time.deltaTime;
+	}
+}
+
+function showTime(time : float){
+	var minutes : int = 0;
+
+	while(time >= 60){
+		time -= 60;
+		minutes++;
+	}
+	var seconds : int = Mathf.Floor(time);
+	if(minutes < 10)
+		text_time.text = " ";
+	text_time.text = minutes.ToString() + ":";
+	if(seconds < 10)
+		text_time.text = text_time.text + "0";
+	text_time.text = text_time.text + seconds.ToString();
+
+	text_time.color = (overTime)? new Color(255,0,0,1):new Color(0,255,0,1);
+}
---
+++ Assets/Scripts/Movement.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+public var maxMovementSpeed : float = 5;
+public var moveForce : float = 1;
+public var jumpForce : float = 10;
+public var jumping : boolean = false;
+
+public var rb : Rigidbody2D;
+public var horizontalMovement : float = 0;
+
+public var speedx : float;
+public var speedy : float;
+
+function Start () {
+}
+
+function Update () {
+	horizontalMovement = Input.GetAxisRaw("Horizontal");
+	rb.AddForce(new Vector2(horizontalMovement * moveForce, 0));
+	if(Input.GetKeyDown("space") && !jumping){
+		rb.AddForce(new Vector2(.0f, jumpForce),ForceMode2D.Impulse);
+		jumping = true;
+	}
+	capSpeed();
+	speedx = rb.velocity.x;
+	speedy = rb.velocity.y;
+}
+
+function capSpeed(){
+	if( rb.velocity.x > maxMovementSpeed){
+		rb.velocity = new Vector2(maxMovementSpeed,rb.velocity.y);
+	}else if( rb.velocity.x < -maxMovementSpeed){
+		rb.velocity = new Vector2(-maxMovementSpeed,rb.velocity.y);
+	}
+}
+
+function OnCollisionEnter2D(collision2D: Collision2D) {
+	if(collision2D.gameObject.tag == "Floor")
+		jumping = false;
+}
