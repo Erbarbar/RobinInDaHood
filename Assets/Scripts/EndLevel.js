58fb601b5d07ca2a6daca13ff52e78c76643a70b
Pol Kihn <pol.kihn.001@student.uni.lu>
2017-05-17 20:16:31 +0100

    pull prepareations

---
+++ Assets/Scripts/EndLevel.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D(coll: Collider2D){
+	if((coll.gameObject.name == "Player")&&Input.GetKeyDown ("e")){
+		endLevel();
+	}
+}
+
+function endLevel(){
+
+}
---
+++ Assets/Scripts/Water.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+public var health : Health;
+
+
+function Start () {
+	var player = GameObject.Find("Player") as GameObject;
+	health=player.GetComponent("Health") as Health;
+}
+
+function OnTriggerEnter2D(coll: Collider2D){
+	if(coll.gameObject.name == "Player"){
+		while(health.currentHealth>0){
+			health.takeDamage(1);
+		}
+	}
+}
