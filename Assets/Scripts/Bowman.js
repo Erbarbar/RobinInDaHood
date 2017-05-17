b5f587a93d0dd592a6e9d0c30b23771f12d34177
Erbarbar <filipelopez3@gmail.com>
2017-05-17 20:13:53 +0100

    Add guard animations

---
+++ Assets/Scripts/Bowman.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var bowPrefab : GameObject;
+public var arrowPrefab : GameObject;
+public var range : float;
+public var damage : int;
+public var cooldown : float;
+private var cooldownTimer : float;
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
